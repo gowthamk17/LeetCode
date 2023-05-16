abf2dc5c1f66077ffaa4347d599302b78d8a6155
Gowtham kumar <51103345+gowthamk17@users.noreply.github.com>
2023-05-16 22:01:47 +0530

    Time: 22 ms (27.24%), Space: 15.3 MB (9.98%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
@@ -1,27 +1,22 @@
 class Solution {
 public:
     vector<int> topKFrequent(vector<int>& nums, int k) {
-            std::unordered_map<int, int> umap;
-            int numsSize = nums.size(); 
-            for (int i = 0; i < numsSize; i++)
-            {
-                umap[nums[i]]++;
+        int n = nums.size(); 
+        std::unordered_map<int, int> umap;  
+        for (int i = 0; i < n; i++) {
+            umap[nums[i]]++;
+        }
+        vector<vector<int>> bucket(n+1);
+        for(auto it = umap.begin(); it != umap.end(); ++it) {
+            bucket[it->second].push_back(it->first);
+        }
+        std::vector<int> topKFreq;
+        for(int i = n; i >= 0; --i) {
+            if(topKFreq.size() >= k) break;
+            if(!bucket[i].empty()){
+                topKFreq.insert(topKFreq.end(), bucket[i].begin(), bucket[i].end());
             }
-            std::stack<int> arr[numsSize + 1];
-            for (const auto &[key, value] : umap)
-            {
-                arr[value].push(key);
-            }
-            std::vector<int> topKFreq;
-            for(int i = numsSize, j=k; i >= 0; --i) {
-                while(!arr[i].empty()) {
-                    topKFreq.push_back(arr[i].top());
-                    arr[i].pop();
-                    --j;
-                    if(j == 0) break;
-                }
-                if(j == 0) break;
-            }
-            return topKFreq;
+        }
+        return topKFreq;
     }
 };
