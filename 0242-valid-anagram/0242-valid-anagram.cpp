4c480f7bdc68d195f64f0506db5a94d52443ed35
Gowtham kumar <51103345+gowthamk17@users.noreply.github.com>
2023-06-01 21:50:43 +0530

    Time: 9 ms (73.21%), Space: 7.3 MB (82.26%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -4,18 +4,6 @@ public:
         if(s.length() != t.length()) {
             return false;
         }
-        
-        // solution 1
-        // sort(s.begin(), s.end());
-        // sort(t.begin(), t.end());
-        // for(int i = 0; i < s.length(); i++)  {
-        //     if(s[i] != t[i]) {
-        //         return false;
-        //     }
-        // }
-        // return true;
-        
-        // solution 2
         int freq[26] = {0};
         for(int i = 0; i < s.length(); i++) {
             freq[s[i] - 'a']++;
