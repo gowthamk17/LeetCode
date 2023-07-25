7cd900acc8dbf1bf1d21dc0039b0804d13178d32
Gowtham kumar <51103345+gowthamk17@users.noreply.github.com>
2023-07-25 07:25:17 +0530

    Time: 47 ms (61.52%), Space: 16.3 MB (55.68%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.py
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -3,7 +3,7 @@ class Solution:
         pdic = {"(": ")", "[":"]", "{":"}"}
         stack = list()
         for i in range(len(s)):
-            if s[i] == "(" or s[i] == "["  or s[i] == "{":
+            if s[i] in pdic:
                 stack.append(s[i])
             elif stack and pdic[stack[-1]] == s[i]:
                 stack.pop()
