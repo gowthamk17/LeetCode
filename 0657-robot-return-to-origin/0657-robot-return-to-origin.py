03c8c41eedf6415afa6da32c117eaad7a3157228
Gowtham kumar <51103345+gowthamk17@users.noreply.github.com>
2023-07-30 21:47:18 +0530

    Time: 56 ms (81.21%), Space: 16.5 MB (29.05%) - LeetHub

--- 0657-robot-return-to-origin/0657-robot-return-to-origin.py
+++ 0657-robot-return-to-origin/0657-robot-return-to-origin.py
@@ -1,16 +1,14 @@
 class Solution:
     def judgeCircle(self, moves: str) -> bool:
-        coords = [0,0]
+        x = 0
+        y = 0
         for char in moves:
             if char == 'R':
-                coords[0] += 1
+                x += 1
             elif char == 'L':
-                coords[0] -= 1
+                x -= 1
             elif char == 'U':
-                coords[1] += 1
+                y += 1
             elif char == 'D':
-                coords[1] -= 1
-        if coords[0] == 0 and coords[1] == 0:
-            return True
-        else:
-            return False
+                y -= 1
+        return x == 0 and y == 0
