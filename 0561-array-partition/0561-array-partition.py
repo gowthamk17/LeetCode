678872d9d94c64ddf2985e4b312f71871213b46b
Gowtham kumar <51103345+gowthamk17@users.noreply.github.com>
2023-08-05 08:04:26 +0530

    Time: 236 ms (96.38%), Space: 19.4 MB (44.16%) - LeetHub

---
+++ 0561-array-partition/0561-array-partition.py
@@ -0,0 +1,7 @@
+class Solution:
+    def arrayPairSum(self, nums: List[int]) -> int:
+        nums.sort()
+        maxSum = 0
+        for i in range(0, len(nums), 2):
+            maxSum += nums[i]
+        return maxSum
