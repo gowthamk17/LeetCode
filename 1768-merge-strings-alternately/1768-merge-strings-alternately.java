61d5cfaa6ba0176ee42d5442d824996c3a6f2d74
Gowtham kumar <51103345+gowthamk17@users.noreply.github.com>
2023-08-24 07:27:11 +0530

    Time: 0 ms (100.00%), Space: 40.5 MB (93.87%) - LeetHub

--- 1768-merge-strings-alternately/1768-merge-strings-alternately.java
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.java
@@ -1,17 +1,35 @@
 class Solution {
     public String mergeAlternately(String word1, String word2) {
-        String mergedString = "";
-        int mergeLimit = Math.min(word1.length(), word2.length());
-        for(int i = 0; i < mergeLimit; i++) {
-            mergedString += word1.charAt(i);
-            mergedString += word2.charAt(i);
+        StringBuilder mergedString = new StringBuilder();
+        int m = word1.length();
+        int n = word2.length();
+        int i = 0;
+        int j = 0;
+        while(i < m || j < n) {
+            if(i < m) {
+                mergedString.append(word1.charAt(i));
+                i++;
+            }
+            if(j < n) {
+                mergedString.append(word2.charAt(j));
+                j++;
+            }
         }
-        if(word1.length() >= mergeLimit) {
-            mergedString += word1.substring(mergeLimit);
-        }
-        if(word2.length() >= mergeLimit) {
-            mergedString += word2.substring(mergeLimit);
-        }
-        return mergedString;
+        return mergedString.toString();
+        
+        // Intuition
+        // String mergedString = "";
+        // int mergeLimit = Math.min(word1.length(), word2.length());
+        // for(int i = 0; i < mergeLimit; i++) {
+        //     mergedString += word1.charAt(i);
+        //     mergedString += word2.charAt(i);
+        // }
+        // if(word1.length() >= mergeLimit) {
+        //     mergedString += word1.substring(mergeLimit);
+        // }
+        // if(word2.length() >= mergeLimit) {
+        //     mergedString += word2.substring(mergeLimit);
+        // }
+        // return mergedString;
     }
 }
