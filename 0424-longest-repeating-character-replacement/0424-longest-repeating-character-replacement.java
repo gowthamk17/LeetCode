bd1f973edfc78a2dd6f3ac1d2720051c0a09ac96
Gowtham kumar <51103345+gowthamk17@users.noreply.github.com>
2023-08-28 08:12:32 +0530

    Time: 29 ms (25.33%), Space: 43.4 MB (28.48%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.java
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.java
@@ -3,11 +3,13 @@ class Solution {
         HashMap<Character, Integer> countMap = new HashMap<>();
         int result = 0;
         int left = 0;
+        int maxf = 0;
         for (int right = 0; right < s.length(); right++) {
             char c = s.charAt(right);
             int count = countMap.getOrDefault(c, 0);
             countMap.put(c, count+1);
-            if((right-left+1) - Collections.max(countMap.values()) > k) {
+            maxf = Math.max(maxf, countMap.get(c));
+            if(((right-left+1) - maxf) > k) {
                 countMap.put(s.charAt(left), countMap.get(s.charAt(left))-1);
                 left++;
             }
