cd573c7b8906a9cfeebf77731b4fa892246fed81
Gowtham kumar <51103345+gowthamk17@users.noreply.github.com>
2023-09-25 08:25:40 +0530

    Time: 0 ms (100.00%), Space: 6.6 MB (12.28%) - LeetHub

--- 0202-happy-number/0202-happy-number.cpp
+++ 0202-happy-number/0202-happy-number.cpp
@@ -1,19 +1,32 @@
 class Solution {
 public:
     bool isHappy(int n) {
-        if(n < 10) {
-            if(n==1 || n==7) {
-                return true;
-            } else {
-                return false;
-            }
+        // if(n < 10) {
+        //     if(n==1 || n==7) {
+        //         return true;
+        //     } else {
+        //         return false;
+        //     }
+        // }
+        // int squareSum = getSquareSum(n);
+        // return isHappy(squareSum);
+        
+        set<int> visited;
+        while(true) {
+            n = getSquareSum(n);
+            if(n==1) return true;
+            if(visited.find(n) != visited.end()) return false;
+            visited.insert(n);
         }
+        return false;
+    }
+    int getSquareSum(int n) {
         int sum = 0;
         while(n) {
             int x = n % 10;
             sum += (x * x);
             n/=10;
         }
-        return isHappy(sum);
+        return sum;
     }
 };
