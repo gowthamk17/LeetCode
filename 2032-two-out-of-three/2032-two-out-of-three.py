26c7d6205b5772be3825fbf722d6c0ddb5e27564
Gowtham kumar <51103345+gowthamk17@users.noreply.github.com>
2023-11-09 07:00:11 +0530

    Time: 67 ms (84.92%), Space: 16.3 MB (19.05%) - LeetHub

--- 2032-two-out-of-three/2032-two-out-of-three.py
+++ 2032-two-out-of-three/2032-two-out-of-three.py
@@ -1,13 +1,16 @@
 class Solution:
     def twoOutOfThree(self, nums1: List[int], nums2: List[int], nums3: List[int]) -> List[int]:
         ans = []
+        set1 = set(nums1)
+        set2 = set(nums2)
+        set3 = set(nums3)
         for n in nums1:
-            if n in nums2 and n not in ans:
+            if n in set2 and n not in ans:
                 ans.append(n)
         for n in nums2:
-            if n in nums3 and n not in ans:
+            if n in set3 and n not in ans:
                 ans.append(n)
         for n in nums3:
-            if n in nums1 and n not in ans:
+            if n in set1 and n not in ans:
                 ans.append(n)
         return ans
