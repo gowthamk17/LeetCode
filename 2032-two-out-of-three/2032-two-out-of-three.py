82162e5926d55a203a4a85366a183102e4f7da28
Gowtham kumar <51103345+gowthamk17@users.noreply.github.com>
2023-11-09 07:27:17 +0530

    Time: 76 ms (38.29%), Space: 16.2 MB (54.46%) - LeetHub

--- 2032-two-out-of-three/2032-two-out-of-three.py
+++ 2032-two-out-of-three/2032-two-out-of-three.py
@@ -5,12 +5,12 @@ class Solution:
         nums2 = set(nums2)
         nums3 = set(nums3)
         for n in nums1:
-            if n in nums2:
+            if n in nums2 and n not in ans:
                 ans.append(n)
         for n in nums2:
-            if n in nums3:
+            if n in nums3 and n not in ans:
                 ans.append(n)
         for n in nums3:
-            if n in nums1:
+            if n in nums1 and n not in ans:
                 ans.append(n)
-        return list(set(ans))
+        return ans
