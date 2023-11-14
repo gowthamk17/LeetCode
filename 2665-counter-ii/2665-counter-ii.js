042122c1e3af6f3bd591a608525d21b8f7db02c3
Gowtham kumar <51103345+gowthamk17@users.noreply.github.com>
2023-11-14 07:14:40 +0530

    Time: 61 ms (41.90%), Space: 44.9 MB (15.47%) - LeetHub

---
+++ 2665-counter-ii/2665-counter-ii.js
@@ -0,0 +1,28 @@
+/**
+ * @param {integer} init
+ * @return { increment: Function, decrement: Function, reset: Function }
+ */
+var createCounter = function(init) {
+    let current = init
+    return {
+        increment: () => {
+            current++
+            return current
+        },
+        decrement: () => {
+            current--
+            return current
+        },
+        reset: () => {
+            current = init
+            return init
+        }
+    }
+};
+
+/**
+ * const counter = createCounter(5)
+ * counter.increment(); // 6
+ * counter.reset(); // 5
+ * counter.decrement(); // 4
+ */
