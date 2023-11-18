bb2784f3cca221400e2aba80d08d7b8c177af70c
Gowtham kumar <51103345+gowthamk17@users.noreply.github.com>
2023-11-18 08:03:00 +0530

    Time: 64 ms (51.05%), Space: 43.5 MB (24.12%) - LeetHub

--- 2629-function-composition/2629-function-composition.js
+++ 2629-function-composition/2629-function-composition.js
@@ -5,12 +5,12 @@
 var compose = function(functions) {
     
 	return function(x) {
-        // for (let i = 0; i < functions.length; i++) {
-        //     x = functions[functions.length - 1 - i](x)
-        // }
-        if (functions.length) {
-            return functions.reverse().reduce((x, fn) => fn(x), x)
+        for (let i = 0; i < functions.length; i++) {
+            x = functions[functions.length - 1 - i](x)
         }
+        // if (functions.length) {
+        //     return functions.reverse().reduce((x, fn) => fn(x), x)
+        // }
         return x
     }
 };
