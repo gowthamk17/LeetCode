c500c8d59e2e0a01171b5bd7ebfc60ef76881e42
Gowtham kumar <51103345+gowthamk17@users.noreply.github.com>
2023-12-16 10:07:04 +0530

    Time: 51 ms (71.87%), Space: 17.7 MB (9.94%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,11 +1,3 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        charMap = [0 for _ in range(26)]
-        for c in s:
-            charMap[ord(c) - ord('a')] += 1
-        for c in t:
-            charMap[ord(c) - ord('a')] -= 1
-        for c in charMap:
-            if c:
-                return False
-        return True
+        return sorted(s) == sorted(t)
