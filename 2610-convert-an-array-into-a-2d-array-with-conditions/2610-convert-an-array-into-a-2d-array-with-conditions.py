ce8a8272d480b1dccac59cac08cc66abdcd3b376
Gowtham kumar <51103345+gowthamk17@users.noreply.github.com>
2024-01-02 08:04:46 +0530

    Time: 100 ms (5.01%), Space: 17.1 MB (22.62%) - LeetHub

---
+++ 2610-convert-an-array-into-a-2d-array-with-conditions/2610-convert-an-array-into-a-2d-array-with-conditions.py
@@ -0,0 +1,13 @@
+class Solution:
+    def findMatrix(self, nums: List[int]) -> List[List[int]]:
+        count = defaultdict(int)
+        res = []
+        
+        for n in nums:
+            row = count[n]
+            if row == len(res):
+                res.append([])
+            res[row].append(n)
+            count[n] += 1
+        
+        return res
