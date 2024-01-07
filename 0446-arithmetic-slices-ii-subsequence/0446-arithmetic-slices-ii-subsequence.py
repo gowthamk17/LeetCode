c85f2d69e4feafb6d5650dd4083bffe38f902e41
Gowtham kumar <51103345+gowthamk17@users.noreply.github.com>
2024-01-07 10:59:16 +0530

    Time: 643 ms (57.20%), Space: 72 MB (25.90%) - LeetHub

---
+++ 0446-arithmetic-slices-ii-subsequence/0446-arithmetic-slices-ii-subsequence.py
@@ -0,0 +1,11 @@
+class Solution:
+    def numberOfArithmeticSlices(self, nums: List[int]) -> int:
+        res, n = 0, len(nums)
+        dp = [defaultdict(int) for _ in range(n)]
+
+        for i in range(n):
+            for j in range(i):
+                diff = nums[i] - nums[j]
+                dp[i][diff] += 1 + dp[j][diff]
+                res += dp[j][diff]
+        return res
