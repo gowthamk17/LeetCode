ecc7134f65276086211a1cb700549d42f11489ac
Gowtham kumar <51103345+gowthamk17@users.noreply.github.com>
2024-01-15 08:05:45 +0530

    Time: 1419 ms (84.37%), Space: 72.1 MB (35.64%) - LeetHub

--- 2225-find-players-with-zero-or-one-losses/2225-find-players-with-zero-or-one-losses.py
+++ 2225-find-players-with-zero-or-one-losses/2225-find-players-with-zero-or-one-losses.py
@@ -1,18 +1,18 @@
 class Solution:
     def findWinners(self, matches: List[List[int]]) -> List[List[int]]:
         loserMap = defaultdict(int)
-        player = set()
-        for match in matches:
-            player.add(match[0])
-            player.add(match[1])
-            loserMap[match[1]] += 1
-        
+        for winner, loser in matches:
+            if loserMap[winner] == 0:
+                loserMap[winner] = -1
+            if loserMap[loser] == -1:
+                loserMap[loser] = 1
+            else:
+                loserMap[loser] += 1
         only_winners = []
-        for p in player:
-            if p not in loserMap:
-                only_winners.append(p)
         one_loss = []
         for loser in loserMap:
+            if loserMap[loser] == -1:
+                only_winners.append(loser)
             if loserMap[loser] == 1:
                 one_loss.append(loser)
         return [sorted(only_winners), sorted(one_loss)]
